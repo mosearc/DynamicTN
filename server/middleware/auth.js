ecf6e5f7766fa20c42e6194bd0eb91532ace80c1
Mosè Arcaro <mose.arcaro@gmail.com>
2024-05-08 11:16:06 +0200

    Delete server directory

--- server/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-function auth(req, res, next){
-    if(req.user?.id) return next()
-
-    return res.sendStatus(401)
-}
-
-module.exports = auth
--- server/middleware/error_handler.js
+++
@@ -1,5 +0,0 @@
-function errorHandler(err, req, res, next){
-    res.status(500).send(err.message)
-}
-
-module.exports = errorHandler
