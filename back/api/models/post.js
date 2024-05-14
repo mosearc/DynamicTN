b78d29387654fa468cc87cc49e6fe50911427899
Mattia Santaniello <86574488+zerise000@users.noreply.github.com>
2024-05-14 23:24:09 +0200

    Update post.js

--- back/api/models/post.js
+++ back/api/models/post.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-    _id: mongoose.Schema.ObjectId,
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    location: { type: String, required: true },
-	votes: {type:Number, required:true},
-    image: {type: String, default: null },
-    //price: { type: Number, require: true } //deve essere un numero ed è necessario
-},{timestamps:true});
+    title: {
+        type: String,
+        required: true,
+    },
 
-module.exports = mongoose.model("Post", postSchema);
+    description: {
+        type: String,
+        required: true,
+    },
+
+    location: {
+        type: String,
+        required: true,
+    },
+    
+    image: {
+        type: String,
+        required: false,
+    },
+}, {
+    timestamps: true,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
