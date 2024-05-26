c0d0dfaea6dfdb9e6e3fffcb9ede226dbc356c53
Mosè Arcaro <mose.arcaro@gmail.com>
2024-05-26 20:31:54 +0200

    Update LoginView.vue

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -47,7 +47,7 @@ export default {
 
     const submit = async () => {
 
-      const response = await fetch('https://dynamictn-back-main.onrender.com', {
+      const response = await fetch('https://dynamictn-back-main.onrender.com/auth', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         credentials: 'include',
